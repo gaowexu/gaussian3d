17fd3dbe73f2e9acc02bb94e2779a090d7db0cf5
Gavin Xu <Gavin.Xu@smart.com>
2024-04-17 17:54:12 +0800

    add diff gaussian rasterization

---
+++ submodules/diff-gaussian-rasterization/cuda_rasterizer/config.h
@@ -0,0 +1,19 @@
+/*
+ * Copyright (C) 2023, Inria
+ * GRAPHDECO research group, https://team.inria.fr/graphdeco
+ * All rights reserved.
+ *
+ * This software is free for non-commercial, research and evaluation use
+ * under the terms of the LICENSE.md file.
+ *
+ * For inquiries contact  george.drettakis@inria.fr
+ */
+
+#ifndef SUBMODULES_DIFF_GAUSSIAN_RASTERIZATION_CUDA_RASTERIZER_CONFIG_H
+#define SUBMODULES_DIFF_GAUSSIAN_RASTERIZATION_CUDA_RASTERIZER_CONFIG_H
+
+#define NUM_CHANNELS 3  // Default 3, RGB
+#define BLOCK_X 16
+#define BLOCK_Y 16
+
+#endif  // SUBMODULES_DIFF_GAUSSIAN_RASTERIZATION_CUDA_RASTERIZER_CONFIG_H
